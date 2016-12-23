f46142da0784cb059ae39d0dcc1ff1daf79ffbc8
张泽谋 <1269199459@qq.com>
2016-12-23 13:05:47 +0800

    HelloWorld

--- src/com/szrt/java/zhangzemou/HelloWorld.java
+++ src/com/szrt/java/zhangzemou/HelloWorld.java
@@ -3,7 +3,7 @@ package com.szrt.java.zhangzemou;
 public class HelloWorld {
 	
 	public static void main(String[] args) {
-		System.out.println("HelloWorld");
+		System.out.println("广东海洋大学寸金学院");
 	}
 
 }
